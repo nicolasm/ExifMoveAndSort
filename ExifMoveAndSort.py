f5118bde8ab8333fa500aafd0fdf9aa49646bf4b
Nicolas Meier <nicolasmeier.nm@gmail.com>
2013-10-07 18:53:43 +0200

    Write ExifMoveAndSort in Python

---
+++ ExifMoveAndSort.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+# ExifMoveAndSort by Nicolas Meier
+# Move and sort pictures by using Exiftool and Hazel
+# https://github.com/nicolasm/ExifMoveAndSort
+# Last revsion: Oct 6, 2013
+# Pictures are moved to the Photos repository and sorted by Exif original date
+# ExifTool (http://www.sno.phy.queensu.ca/~phil/exiftool/)
+
+import re
+import subprocess
+import gntp.notifier
+import sys
+import os
+import locale
+
+def is_running(process):
+    s = subprocess.Popen(["ps", "axw"],stdout=subprocess.PIPE)
+    for x in s.stdout:
+        if re.search(process, x):
+            return True
+
+    return False
+
+file = sys.argv[1]
+path = os.path.dirname(os.path.abspath(file))
+
+target = ""
+# Set the final directory according to the source
+if os.path.samefile(path, "/Users/YourUser/path/to/Camera Roll"):
+    target = "iPhone"
+elif os.path.samefile(path, "/Users/YourUser/path/to/incoming"):
+     target = ""
+
+# Force the locale to fr_FR.UTF-8 because Hazel use the wrong locale
+env = os.environ.copy()
+env['LANG'] = 'fr_FR.UTF-8'
+
+# Ensure Phoenix Slides and PhotoSync are not running
+if not (is_running("Phoenix Slides\.app") or is_running("PhotoSync\.app")):
+    print 1
+    s = subprocess.Popen(["exiftool", "-Directory<DateTimeOriginal", "-d", "/path/to/target/%Y/%m - %B/" + target, path], stdout=subprocess.PIPE, env = env)
+    message = ""
+    for x in s.stdout:
+        message = message + x
+
+    gntp.notifier.mini(message)
